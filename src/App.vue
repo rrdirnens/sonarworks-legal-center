f396105e5de4d9f0df7109928adbf50a5bca2d51
Rob Dirnens <42729985+rrdirnens@users.noreply.github.com>
2021-05-20 10:08:37 +0300

    replace colors with vars, change package version for netlify

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ export default {
   name: "App",
   components: {
     HeaderComponent,
-    FooterComponent,
+    FooterComponent
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'bootstrap'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
